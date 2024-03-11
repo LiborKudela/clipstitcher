5c0ef584d60bb2e0e8541017cd94a710775a9ccd
Libor Kudela <libor.kudela1@gmail.com>
2024-03-11 09:49:38 +0100

    Adding LinearTransfor and LinearTransition

--- clipstitcher/__init__.py
+++ clipstitcher/__init__.py
@@ -2,6 +2,7 @@ from .scene_objects import (
     default_options,
     Image, Html_page, Tweet, 
     Video, Overlay, Scene_sequence,
+    LinearTransform, LinearTransition,
     )
 
 from .client_sync import (
